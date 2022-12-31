e214a4610b20ea9bf8e8ecde64db7aa7dfd0c9e6
Tony Anciaux <tonyanciaux@gmail.com>
2022-12-31 13:36:56 +0100

    implemented checking answer and moving to box

--- cards/views.py
+++ cards/views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import (
     ListView,
     CreateView,
@@ -6,6 +7,9 @@ from django.views.generic import (
 )
 
 from .models import Card
+from .forms import CardCheckForm
+
+import random
 
 
 class CardListView(ListView):
@@ -25,12 +29,23 @@ class CardUpdateView(CardCreateView, UpdateView):
 
 class BoxView(CardListView):
     template_name = "cards/box.html"
+    form_class = CardCheckForm
 
     def get_queryset(self):
         return Card.objects.filter(box=self.kwargs['box_num'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["box_number"] = self.kwargs["box_number"]
+        context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = random.choice(self.object_list)
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+            card.move(form.cleaned_data["solved"])
+
+        return redirect(request.META.get('HTTP_REFERER'))
     
